aab8b268f7703c6f7603d45cf9b44bccbccb3038
Jhairo Rivera <jhairorivera@Jhairos-MacBook-Pro.local>
2020-07-01 12:17:42 -0500

    Updates

---
+++ precache-manifest.79f6808912358862f54d9a869647680d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6373abc52ed666df6c6425a45c36aecd",
+    "url": "/react-twitter/index.html"
+  },
+  {
+    "revision": "f1485d10941dfe8fba50",
+    "url": "/react-twitter/static/css/2.9f98d992.chunk.css"
+  },
+  {
+    "revision": "b016a84d739c7ad322ce",
+    "url": "/react-twitter/static/css/main.132a36d4.chunk.css"
+  },
+  {
+    "revision": "f1485d10941dfe8fba50",
+    "url": "/react-twitter/static/js/2.dd6a9bde.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/react-twitter/static/js/2.dd6a9bde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b016a84d739c7ad322ce",
+    "url": "/react-twitter/static/js/main.6b97f6af.chunk.js"
+  },
+  {
+    "revision": "956ddba97b238fe92335",
+    "url": "/react-twitter/static/js/runtime-main.b228dca6.js"
+  },
+  {
+    "revision": "16e35662baa4ca9e4f4344fd51fb7ec6",
+    "url": "/react-twitter/static/media/camara.16e35662.svg"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/react-twitter/static/media/close.cad70081.svg"
+  },
+  {
+    "revision": "9b45fe492d3b935b1a283ece2e1b5ac8",
+    "url": "/react-twitter/static/media/date.9b45fe49.svg"
+  },
+  {
+    "revision": "d3c57e3b34eea06328b31dbb81bcabc4",
+    "url": "/react-twitter/static/media/error-404.d3c57e3b.png"
+  },
+  {
+    "revision": "a108520eea84cd603eeaf8eeda7676b1",
+    "url": "/react-twitter/static/media/link.a108520e.svg"
+  },
+  {
+    "revision": "02f2070b4161235fe7d8909276457b53",
+    "url": "/react-twitter/static/media/location.02f2070b.svg"
+  },
+  {
+    "revision": "1769587d7e1330e9d4241c30f672c7ff",
+    "url": "/react-twitter/static/media/logo.1769587d.png"
+  },
+  {
+    "revision": "41f65f75cb0b41db6fe44ab4b074bacc",
+    "url": "/react-twitter/static/media/logoWhite.41f65f75.png"
+  }
+]);
